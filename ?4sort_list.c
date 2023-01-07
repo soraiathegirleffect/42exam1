acb8c480b8713e7303a23c8dc280199f15eab727
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-01-07 17:57:49 +0000

    Update and rename ?4rostring.c to ?4sort_list.c

--- ?4sort_list.c
+++ ?4sort_list.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "list.h"
 
-*/ sort_list.list.h
+/* sort_list.list.h
 typedef struct s_list t_list;
 
 struct s_list
@@ -9,7 +9,7 @@ struct s_list
 	int     data;
 	t_list  *next;
 }; 
-/*
+*/
 
 
 t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
