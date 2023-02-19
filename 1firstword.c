3ba7fac418725aa7b2f5531b63a4c6b8a47a374c
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-02-19 19:37:03 +0000

    Update 1firstword.c

--- 1firstword.c
+++ 1firstword.c
@@ -7,9 +7,9 @@ int main (int ac, char **av)
 	i = 0;
 	if (ac == 2)
 	{
-		while (av[1][i] == ' ' || av[1][i] == '\t')
+		while (av[1][i] == ' ' || av[1][i] == '\t' || av[1][i] == '\t')
 			i++;
-		while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
+		while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t' && av[1][i] != '\t')
 		{
 			write(1, &av[1][i], 1);
 			i++;
