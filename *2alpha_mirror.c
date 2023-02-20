99f058f8ba168ead6cafa6acb3e716c21bc9f6ab
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-02-20 23:09:59 +0000

    Update *2alpha_mirror.c

--- *2alpha_mirror.c
+++ *2alpha_mirror.c
@@ -10,9 +10,9 @@ int	main(int ac, char **av)
 		while (av[1][i] != '\0')
 		{
 			if (av[1][i] >= 'a' && av[1][i] <= 'z')
-				av[1][i] = 122 - av[1][i] + 97;
+				av[1][i] = 122 - av[1][i] + 97;    // ou 219 - av
 			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				av[1][i] = 90 - av[1][i] + 65;
+				av[1][i] = 90 - av[1][i] + 65;     // oy 155 - av
 			write(1, &av[1][i], 1);
 			i++;
 		}
