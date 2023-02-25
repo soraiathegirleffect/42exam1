83a702edac9ba040dd3e0b338ea586461a5cfc07
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-02-25 17:25:29 +0000

    Update 3expand_str.c

--- 3expand_str.c
+++ 3expand_str.c
@@ -1,5 +1,49 @@
 #include <unistd.h>
 
+int wordlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] && str[i] != ' ' && str[i] != '\t')
+		i++;
+	return (i);
+}
+
+int threespstr(char *str)
+{
+	int len;
+	int word = 1;
+
+	while (*str)
+	{
+		while (*str == ' ' || *str == '\t')
+			str++;
+		len = wordlen(str);
+		if (len > 0 && word == 0)
+			write(1, "   ", 3);
+		write(1, str, len);
+		word = 0;
+		str = str + len;
+	}
+}
+
+int main (int ac, char **av)
+{
+	if (ac == 2)
+		threespstr(av[1]);
+	
+	write(1, "\n", 1);
+	return (0);
+}
+
+
+
+// orrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+
+
+
+#include <unistd.h>
+
 int		word_len(char *str)
 {
 	int i = 0;
