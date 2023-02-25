57347ddfd28a09ecda1700f63c9143260109e00c
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-02-25 17:57:23 +0000

    Update *1revprint.c

--- *1revprint.c
+++ *1revprint.c
@@ -1,31 +1,50 @@
 #include <unistd.h>
 
-int	ft_strlen(char *str)
+int main (int ac, char **av)
 {
-	int	i;
+	int i = 0;
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*ft_rev_print(char *str)
-{
-	int	i;
-
-	i = ft_strlen(str) - 1;
-	while (i >= 0)
+	if (ac == 2)
 	{
-		write(1, &str[i], 1);
-		i--;
+		while(av[1][i])
+			i++;
+		i = i - 1;
+		while(i >= 0)
+		{
+			write(1, &av[1][i], 1);
+			i--;
+		}
 	}
-	return (str);
+	write(1, "\n", 1);
+	return (0);
 }
-/*
- * int main()
+
+
+
+
+
+
+//orrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+
+
+
+#include <unistd.h>
+
+int	main(int argc, char *argv[])
 {
-	char str[] = "gbrireugneb";
-	ft_rev_print(str);
+	int	i = 0;
+
+	if (argc == 2)
+	{
+		while (argv[1][i])
+			i += 1;
+		while (i)
+			write(1, &argv[1][--i], 1);
+	}
+	write(1, "\n", 1);
+	return (0);
 }
-*/
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
