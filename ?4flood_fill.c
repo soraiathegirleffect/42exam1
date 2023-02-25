a224ffdb338f3a53ccc510c35c5a59e419a4c35c
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-02-25 15:04:41 +0000

    Update ?4flood_fill.c

--- ?4flood_fill.c
+++ ?4flood_fill.c
@@ -1,3 +1,38 @@
+
+typedef struct  s_point
+  {
+    int           x;
+    int           y;
+  }               t_point;
+
+void  ffill(char **tab, t_point size, char target, int row, int col)
+{
+	if (row < 0 || col < 0 || row >= size.y || col >= size.x || tab[row][col] != target)
+		return;
+	/*begin.y=row, begin.x=col; The function won't fill diagonally, not negative, not less than size*/
+
+	tab[row][col] = 'F';
+	ffill(tab, size, target, row - 1, col);
+	ffill(tab, size, target, row + 1, col);
+	ffill(tab, size, target, row, col - 1);
+	ffill(tab, size, target, row, col + 1);
+
+	/*// Recursively fill the surrounding spaces*/
+}
+
+void  flood_fill(char **tab, t_point size, t_point begin)
+{
+	char target = tab[begin.y][begin.x];
+	ffill(tab, size, target, begin.y, begin.x);
+}
+
+
+
+
+//orrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+
+
+
 typedef struct	s_point
 {
 	int			x;
