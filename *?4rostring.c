3023016ebf22efae0978b5b960252638d55f1e21
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-02-25 17:27:21 +0000

    Update *?4rostring.c

--- *?4rostring.c
+++ *?4rostring.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
     int i;
     int start;
@@ -10,28 +10,28 @@ int main(int argc, char **argv)
     if (argc > 1)
     {
         i = 0;
-        while (argv[1][i] == ' ' || argv[1][i] == '\t')
+        while (av[1][i] == ' ' || av[1][i] == '\t')
             i++;
         start = i;
-        while (argv[1][i] != '\0' && argv[1][i] != ' ' && argv[1][i] != '\t')
+        while (av[1][i] != '\0' && av[1][i] != ' ' && av[1][i] != '\t')
             i++;
         end = i;
-        while (argv[1][i] == ' ' || argv[1][i] == '\t')
+        while (av[1][i] == ' ' || av[1][i] == '\t')
             i++;
-        while(argv[1][i]) 
+        while(av[1][i]) 
         {
-            while ((argv[1][i] == ' ' && argv[1][i + 1] == ' ') || (argv[1][i] == '\t' && argv[1][i + 1] == '\t'))
+            while ((av[1][i] == ' ' && av[1][i + 1] == ' ') || (av[1][i] == '\t' && av[1][i + 1] == '\t'))
                 i++; 
-            if (argv[1][i] == ' ' || argv[1][i] == '\t')
+            if (av[1][i] == ' ' || av[1][i] == '\t')
                 flag = 1; 
-            write(1, &argv[1][i], 1);
+            write(1, &av[1][i], 1);
             i++;
         }
         if (flag == 1)
             write(1, " ", 1);
         while (start < end)
         {
-            write(1, &argv[1][start], 1);
+            write(1, &av[1][start], 1);
             start++;
         }
     }
