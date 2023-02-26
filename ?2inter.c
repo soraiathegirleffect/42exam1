be1868c5e47f917ded708fa74e8a45334923b9ab
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-02-26 16:54:52 +0000

    Update ?2inter.c

--- ?2inter.c
+++ ?2inter.c
@@ -4,12 +4,12 @@ int	main(int ac, char **av)
 {
 	int	i;
 	int k;
-	int l;
+	int p;
 	char *s1;
 	char *s2;
 
 	i = 0;
-	l = 0;
+	p = 0;
 	if (ac == 3)
 	{
 		s1 = av[1];
@@ -21,15 +21,15 @@ int	main(int ac, char **av)
 			{
 				if(s1[i] == s2[k])
 				{ 
-					l = 0;
-					while (s1[l] != s1[i])
-						l++;
-					if (l == i)
+					p = 0;
+					while (s1[p] != s1[i])
+						p++;
+					if (p == i)
 					{
-						l = 0;
-						while (s2[l] != s2[k])
-							l++;
-						if (l == k)
+						p = 0;
+						while (s2[p] != s2[k])
+							p++;
+						if (p == k)
 							write(1, &s1[i], 1);
 					}
 				}
@@ -40,3 +40,23 @@ int	main(int ac, char **av)
 	}
 	write(1, "\n", 1);
 }
+
+/*
+Write a program that takes two strings and displays, without doubles, the
+characters that appear in both strings, in the order they appear in the first
+one.
+
+The display will be followed by a \n.
+
+If the number of arguments is not 2, the program displays \n.
+
+Examples:
+
+$>./inter "padinton" "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
+padinto$
+$>./inter ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
+df6ewg4$
+$>./inter "rien" "cette phrase ne cache rien" | cat -e
+rien$
+$>./inter | cat -e
+$*/
