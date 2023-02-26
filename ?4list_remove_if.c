a13d6750344ad8157462a326e743cdeeca8d22d2
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-02-26 17:56:09 +0000

    Update ?4list_remove_if.c

--- ?4list_remove_if.c
+++ ?4list_remove_if.c
@@ -1,14 +1,48 @@
+ft_list.h
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct s_list
+{
+	struct s_list	*next;
+	void			*data;
+}				t_list;
+
+#endif
+
+
+
+
+
 #include <stdlib.h>
 #include "ft_list.h"
+#include <stdio.h>
+#include <string.h>
 
-/*
-ft_list.h
-typedef struct    s_list
+
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-    struct s_list *next;
-    void          *data;
-}                 t_list;
-*/
+    t_list *current;
+
+    if (!begin_list || !*begin_list)
+        return ;
+    current = *begin_list;
+    if ((*cmp)(current->data, data_ref) == 0)
+    {
+        *begin_list = current->next;
+        free(current);
+        ft_list_remove_if(begin_list, data_ref, cmp);
+    }
+    current = *begin_list;
+    //current = current->next;              this is going to be error in case current->next is 0
+    ft_list_remove_if(&current->next, data_ref, cmp);
+}
+
+//orrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+
+#include <stdlib.h>
+#include "ft_list.h"
 
 void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
@@ -27,50 +61,9 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 	ft_list_remove_if(&cur->next, data_ref, cmp);
 }
 
-//---------------------------------------------------------------------
-// #include <stdio.h>
-// #include <string.h>
-
-// void	print_list(t_list **begin_list)
-// {
-// 	t_list *cur = *begin_list;
-// 	while (cur != 0)
-// 	{
-// 		printf("%s\n", cur->data);
-// 		cur = cur->next;
-// 	}
-// }
-
-// int		main(void)
-// {
-// 	char straa[] = "String aa";
-// 	t_list *aa = malloc(sizeof(t_list));
-// 	aa->next = 0;
-// 	aa->data = straa;
-
-// 	char strbb[] = "String bb";
-// 	t_list *bb = malloc(sizeof(t_list));
-// 	bb->next = 0;
-// 	bb->data = strbb;
-
-// 	char strcc[] = "String cc";
-// 	t_list *cc = malloc(sizeof(t_list));
-// 	cc->next = 0;
-// 	cc->data = strcc;
-
-// 	char strdd[] = "String dd";
-// 	t_list *dd = malloc(sizeof(t_list));
-// 	dd->next = 0;
-// 	dd->data = strdd;
-
-// 	aa->next = bb;
-// 	bb->next = cc;
-// 	cc->next = dd;
-
-// 	t_list **begin_list = &aa;
-
-// 	print_list(begin_list);
-// 	printf("----------\n");
-// 	ft_list_remove_if(begin_list, straa, strcmp);
-// 	print_list(begin_list);
-// }
+/*Write a function called ft_list_remove_if that removes from the
+passed list any element the data of which is "equal" to the reference data.
+It will be declared as follows :
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)());
+cmp takes two void* and returns 0 when both parameters are equal.
+You have to use the ft_list.h */
