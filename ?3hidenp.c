ab8a061f3061d2cfa66e657836d7e638879a1008
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-02-26 17:19:25 +0000

    Update ?3hidenp.c

--- ?3hidenp.c
+++ ?3hidenp.c
@@ -24,3 +24,11 @@ int		main(int argc, char **argv)
 	write(1, "\n", 1);
 	return (0);
 }
+
+
+/* Write a program named hidenp that takes two strings and displays 1 followed by a newline if the first string is hidden in the second one,
+otherwise displays 0 followed by a newline.
+Let s1 and s2 be strings. We say that s1 is hidden in s2 if it's possible to
+find each character from s1 in s2, in the same order as they appear in s1.
+Also, the empty string is hidden in any string.
+If the number of parameters is not 2, the program displays a newline. */
