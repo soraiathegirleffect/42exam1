67bf19e5b0fdea19c413eac0d38c973ad3aef228
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-02-26 18:22:33 +0000

    Update ?3add_prime_sum.c

--- ?3add_prime_sum.c
+++ ?3add_prime_sum.c
@@ -58,3 +58,19 @@ int		main(int ac, char **av)
 	write(1, "\n", 1);
 	return (0);
 }
+
+
+/*Write a program that takes a positive integer as argument and displays the sum
+of all prime numbers inferior or equal to it followed by a newline.
+If the number of arguments is not 1, or the argument is not a positive number,
+just display 0 followed by a newline.
+Yes, the examples are right.
+Examples:
+$>./add_prime_sum 5
+10
+$>./add_prime_sum 7 | cat -e
+17$
+$>./add_prime_sum | cat -e
+0$
+$>
+*/
