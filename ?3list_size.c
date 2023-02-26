6847a0802226fd163e00c76ae3f5ba504fdab95d
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-02-26 17:33:09 +0000

    Update ?3list_size.c

--- ?3list_size.c
+++ ?3list_size.c
@@ -11,3 +11,17 @@ int	ft_list_size(t_list *begin_list)
 	else
 		return (1 + ft_list_size(begin_list->next));
 }
+
+
+/* write a function that returns the number of elements in the linked list that's
+passed to it.
+It must be declared as follows:
+int	ft_list_size(t_list *begin_list);
+You must use the following structure, and turn it in as a file called
+ft_list.h:
+typedef struct    s_list
+{
+    struct s_list *next;
+    void          *data;
+}                 t_list;
+*/
