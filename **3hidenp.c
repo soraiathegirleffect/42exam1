3a4231669677f0e0e425db7d20ca4b0b62c4b885
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-02-26 17:30:07 +0000

    Update and rename ?3hidenp.c to **3hidenp.c

--- **3hidenp.c
+++ **3hidenp.c
@@ -34,36 +34,6 @@ int main(int ac, char **av)
 	write (1, "\n", 1);
 }
 
-////orrrr
-
-
-#include <unistd.h>
-
-void	hidenp(char *probe, char *target)
-{
-	while (*probe != '\0')
-	{
-		while (*probe != *target && *target != '\0')
-			++target;
-		if (*target == '\0')
-		{
-			write(1, "0", 1);
-			return;
-		}
-		++target;
-		++probe;
-	}
-	write(1, "1", 1);
-}
-
-int		main(int argc, char **argv)
-{
-	if (argc == 3)
-		hidenp(argv[1], argv[2]);
-	write(1, "\n", 1);
-	return (0);
-}
-
 
 /* Write a program named hidenp that takes two strings and displays 1 followed by a newline if the first string is hidden in the second one,
 otherwise displays 0 followed by a newline.
