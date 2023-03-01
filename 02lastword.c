a8654d9fde8e8f554812c224d44df0fe0f5e29fe
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-03-01 18:47:34 +0000

    Update 02lastword.c

--- 02lastword.c
+++ 02lastword.c
@@ -1,3 +1,31 @@
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+	int	j = 0;
+	int i = 0;
+	
+	if (argc == 2)
+	{
+	while (str[i])
+	{
+		if (str[i] == ' ' && str[i + 1] >= 33 && str[i + 1] <= 126)
+			j = i + 1;
+		i++;
+	}
+	while (str[j] >= 33 && str[j] <= 127)
+	{
+		write(1, &str[j], 1);
+		j++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+
+
+
+//orrrrrrrrrrrrrrrrrrrr
+
+
 #include <unistd.h>
 
 int main(int ac, char **av)
