8e6eca50fee89867f79f7b73713a10cd9effdeb9
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-03-05 19:26:26 +0000

    Update 2reverse_bits.c

--- 2reverse_bits.c
+++ 2reverse_bits.c
@@ -11,3 +11,74 @@ unsigned char	reverse_bits(unsigned char octet)
 	}
 	return (res);
 }
+
+
+
+//orrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+Write a function that takes a byte, reverses it, bit by bit (like the
+example) and returns the result.
+Your function must be declared as follows:
+unsigned char	reverse_bits(unsigned char octet);
+Example:
+  1 byte
+_____________
+ 0010  0110
+	 ||
+	 \/
+ 0110  0100
+*/
+
+#include <stdio.h>
+
+unsigned char	reverse_bits(unsigned char octet)
+{
+	unsigned char byte;
+	int i;
+	int n;
+	int power;
+
+	byte = 0;
+	i = 7;
+	while (i >= 0)
+	{
+		if (octet << i & 128)
+		{
+			n = 0;
+			power = 1;
+			if (i == 0)
+				power = 1;
+			while (n < i)
+			{
+				power *= 2;
+				n++;
+			}
+			byte += power;
+		}
+		i--;
+	}
+	return (byte);
+}
+
+/* another form to do */
+unsigned char reverse_bits2(unsigned char octet)
+{
+	int i;
+	unsigned char c;
+
+	c = 0;
+	i = 0;
+	while (i < 4)
+	{
+		if (octet >> i & 1)
+			c += 1 << (7 - i);
+		i++;
+	}
+	i = 0;
+	while (i < 4)
+	{
+		if (octet << i & 128)
+			c += 1 << i;
+		i++;
+	}
+	return (c);
+}
