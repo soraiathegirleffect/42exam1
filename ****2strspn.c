b5e07cc38c0d3a522da735c121fd14f5bc6c552e
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-03-05 19:21:09 +0000

    Create ****2strspn.c

---
+++ ****2strspn.c
@@ -0,0 +1,71 @@
+/*
+Assignment name	: ft_strspn
+Expected files	: ft_strspn.c
+Allowed functions: None
+---------------------------------------------------------------
+Reproduce exactly the behavior of the strspn function
+(man strspn).
+The function should be prototyped as follows:
+size_t	ft_strspn(const char *s, const char *accept);
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+size_t ft_strspn(const char *s, const char *accept)
+{
+    int i;
+    int c;
+    int b;
+
+    i = 0;
+    c = 0;
+    b = 0;
+    while (s[i])
+    {
+        while (accept[c])
+        {
+            if (s[i] == accept[c])
+                break;
+            else if (accept[c + 1] == '\0')
+                return (i);
+            c++;
+        }
+        c = 0;
+        i++;
+    }
+    return (i);
+}
+
+/*other way to do it */
+size_t ft_strspn2(const char *s, const char *accept)
+{
+	size_t i;
+	size_t j;
+	int count;
+	int flag;
+
+	i = 0;
+	j = 0;
+	flag = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				flag = 1;
+				break;
+			}
+			j++;
+		}
+		j = 0;
+		if (flag == 0)
+			break;
+		flag = 0;
+		i++;
+	}
+	return (count);
+}
