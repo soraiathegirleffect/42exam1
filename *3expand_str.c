f5d2afc2ef8e337b554ac8d632fcb0b598a59161
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-03-13 19:42:53 +0000

    Update *3expand_str.c

--- *3expand_str.c
+++ *3expand_str.c
@@ -78,3 +78,53 @@ int		main(int argc, char **argv)
 	write(1, "\n", 1);
 	return (0);
 }
+
+
+// orrrrrrrrrrrrr 
+Write a program that takes a string and displays it with exactly three spaces
+between each word, with no spaces or tabs either at the beginning or the end,
+followed by a newline.
+A word is a section of string delimited either by spaces/tabs, or by the
+start/end of the string.
+If the number of parameters is not 1, or if there are no words, simply display
+a newline.
+Examples:
+$> ./expand_str "See? It's easy to print the same thing" | cat -e
+See?   It's   easy   to   print   the   same   thing$
+$> ./expand_str " this        time it      will     be    more complex  " | cat -e
+this   time   it   will   be   more   complex$
+$> ./expand_str "No S*** Sherlock..." "nAw S*** ShErLaWQ..." | cat -e
+$
+$> ./expand_str "" | cat -e
+$
+$>
+*/
+
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+	int i;
+
+	i = 0;
+	if (ac == 2)
+	{
+		while (av[1][i])
+		{
+			while (av[1][i] == 32 || (av[1][i] >= 9 && av[1][i] <= 13))
+				i++;
+			while (av[1][i] > 32 && av[1][i] < 127)
+			{
+				write (1, &av[1][i], 1);
+				i++;
+			}
+			while (av[1][i] == 32 || (av[1][i] >= 9 && av[1][i] <= 13))
+			{
+				if (av[1][i + 1] > 32 && av[1][i + 1] < 127)
+					write (1, "   ", 3);
+				i++;
+			}
+		}
+	}
+	write (1, "\n", 1);
+}
