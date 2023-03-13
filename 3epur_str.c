190e91a20322cd6feb543c334f558c9ece64fc22
Soraia <113796635+soraiathegirleffect@users.noreply.github.com>
2023-03-13 19:41:30 +0000

    Update 3epur_str.c

--- 3epur_str.c
+++ 3epur_str.c
@@ -1,3 +1,55 @@
+Write a program that takes a string, and displays this string with exactly one
+space between words, with no spaces or tabs either at the beginning or the end,
+followed by a \n.
+A "word" is defined as a part of a string delimited either by spaces/tabs, or
+by the start/end of the string.
+If the number of arguments is not 1, or if there are no words to display, the
+program displays \n.
+Example:
+$> ./epur_str "See? It's easy to print the same thing" | cat -e
+See? It's easy to print the same thing$
+$> ./epur_str " this        time it      will     be    more complex  . " | cat -e
+this time it will be more complex .$
+$> ./epur_str "No S*** Sherlock..." "nAw S*** ShErLaWQ..." | cat -e
+$
+$> ./epur_str "" | cat -e
+$
+$>
+*/
+
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+	int i;
+
+	i = 0;
+	if (ac == 2)
+	{
+		while (av[1][i])
+		{
+			while (av[1][i] == 32 || (av[1][i] >= 9 && av[1][i] <= 13))
+				i++;
+			while (av[1][i] > 32 && av[1][i] < 127)
+			{
+				write (1, &av[1][i], 1);
+				i++;
+			}
+			while (av[1][i] == 32 || (av[1][i] >= 9 && av[1][i] <= 13))
+			{
+				if (av[1][i + 1] > 32 && av[1][i + 1] < 127)
+				{
+					write (1, " ", 1);
+				}
+				i++;
+			}
+		}
+	}
+	write (1, "\n", 1);
+}
+
+// orr mine
+
 #include <unistd.h>
 
 int		skip_whitespace(char *str, int i)
